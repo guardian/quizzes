27af52f8e590bf600883ac734adfd8ca1069f1e4
Robert Berry <robert.berry@theguardian.com>
2015-03-17 11:56:26 +0000

    Move components into single file

---
+++ lib/components.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import map from 'lodash-node/modern/collection/map';
+
+export class Answer extends React.Component {
+    handleAnswer() {
+        console.log(answer);
+    }
+    
+    render() {
+        return <div class="quiz-answer">
+                <li onClick={this.handleAnswer}>{this.props.answer.answer}</li>
+            </div>
+    }
+
+}
+
+export class Question extends React.Component {
+    render() {
+        return <div class="quiz-question">
+            <h4>{this.props.question.question}</h4>
+            <ol>{
+                map(
+                    this.props.question.multiChoiceAnswers,
+                    (answer) => <Answer answer={answer} />
+                )
+            }</ol>
+        </div>
+    }
+}
+
+export class Quiz extends React.Component {
+    render() {
+        return <div>
+            <h2>{this.props.header.titleText}</h2>
+            <p>{this.props.header.trailText}</p>
+            {
+                map(
+                    this.props.questions,
+                    (question) => <Question question={question} />
+                )
+            }
+        </div>
+    }
+}
