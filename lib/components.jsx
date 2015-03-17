6f01667a18d44d70d6f074021923d1354d6873ff
Cantlin Ashrowan <cantlin@ashrowan.com>
2015-03-17 15:15:20 +0000

    nuke the stupid title

--- lib/components.jsx
+++ lib/components.jsx
@@ -102,7 +102,6 @@ export class Quiz extends React.Component {
         }
         
         return <div data-link-name="quiz">
-            <h2>{this.props.header.titleText}</h2>
             <p>{this.props.header.trailText}</p>
             {
                 map(
